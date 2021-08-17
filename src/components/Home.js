50029df6fab504f39f640ccacaa7c014c8e50337
bwennuh <brennabruening@gmail.com>
2021-08-17 12:26:00 -0400

    start set up of components and home page

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render(){
+    return(
+      <div className="home">
+        <div className="about">
+          <h2>{`<a>`}bout me</h2>
+          <p>Hi! I'm Brenna :)</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render(){
+    return(
+      <div className="navbar">
+        <button>Home</button>
+
+        <button>About</button>
+        <button>Projects</button>
+        <button>Br2 Blog</button>
+        <button>Contact</button>
+
+
+      </div>
+    )
+  }
+}
+
+export default Navbar
